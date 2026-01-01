a64f213922e24f7a561d61ca4cffdfe150527e5b
BonClay <shabelnikov06@gmail.com>
2026-01-01 20:27:13 +0300

    Initial Commit

---
+++ NowPlayingWallpaper/NowPlayingWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingWallpaperApp.swift
+//  NowPlayingWallpaper
+//
+//  Created by Александр Шабельников on 01.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NowPlayingWallpaperTests/NowPlayingWallpaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingWallpaperTests.swift
+//  NowPlayingWallpaperTests
+//
+//  Created by Александр Шабельников on 01.01.2026.
+//
+
+import Testing
+@testable import NowPlayingWallpaper
+
+struct NowPlayingWallpaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
